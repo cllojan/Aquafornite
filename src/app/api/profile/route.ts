84327d89ad3ee22e879b44fbe45c328da4e08146
cesarlojan8 <cesarlojan8@gmail.com>
2025-10-15 18:39:03 +0000

    edge runtime

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -30,3 +30,4 @@ export async function GET(){
     }
     return NextResponse.json(user);
 }
+export const runtime = 'edge';
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -14,3 +14,4 @@ export default async function AuthPage(){
     }
     return <AuthClientPage/>
 }
+export const runtime = 'edge';
