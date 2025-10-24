16e6208b697dc9ce3d9256528647da17df99c2d8
cesarlojan8 <cesarlojan8@gmail.com>
2025-10-24 00:25:42 +0000

    webhook

---
+++ src/app/api/webhook/success/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server"
+import Stripe from "stripe";
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2025-08-27.basil"
+})
+
+export async function POST(req: NextRequest) {
+    const sig = req.headers.get("stripe-signature") as string;
+    const body = await req.text();
+
+    try {
+        const event = stripe.webhooks.constructEvent(
+            body,
+            sig,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        )
+
+        if (event.type === "checkout.session.completed") {
+            const session = event.data.object as Stripe.Checkout.Session;
+            const userId = session.metadata?.userId;
+            const items = JSON.parse(session.metadata?.items || "[]");
+            const total = session.amount_total! / 100;
+            await prisma.orders.create({
+                data: {
+                    user_id: Number(userId),
+                    items: JSON.stringify(items),
+                    total: total,
+                },
+            });
+            console.log("Orden guardada");
+        }
+        return NextResponse.json({ received: true }, { status: 200 });
+    } catch (e) {
+        console.error("❌ Error en Webhook:", e.message);
+        return NextResponse.json({ error: "Webhook Error" }, { status: 400 });
+    }
+}
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
