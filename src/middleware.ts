90f88b1f543d94352aaa0be65ed9cdda593c2c4e
cesarlojan8 <cesarlojan8@gmail.com>
2025-10-25 23:37:47 +0000

    xd

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,16 @@
-import { getSessionCookie } from "better-auth/cookies";
-import { NextResponse, type NextRequest } from "next/server";
-
+import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  // ⚙️ Corrige headers inválidos en desarrollo (Cloud Workstations o localhost)
-  if (process.env.NODE_ENV !== "production" && process.env.BETTER_AUTH_URL) {
-    const headers = new Headers(request.headers);
-    const targetHost = new URL(process.env.BETTER_AUTH_URL).host;
-
-    headers.set("x-forwarded-host", targetHost);
-    headers.set("origin", `https://${targetHost}`);
-
-    return NextResponse.next({
-      request: {
-        headers,
-      },
-    });
-  }
-
-  // 🔒 Protege rutas
-  const sessionCookie = getSessionCookie(request);
-  if (!sessionCookie && pathname === "/perfil") {
-    return NextResponse.redirect(new URL("/auth", request.url));
-  }
-
-  return NextResponse.next();
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if(!session) {
+        return NextResponse.redirect(new URL("/auth", request.url));
+    }
+    return NextResponse.next();
 }
-
 export const config = {
-  matcher: ["/perfil","/aquacoins"],
-  
+  runtime: "nodejs",
+  matcher: ["/perfil","/aquacoins"], // Apply middleware to specific routes
 };
