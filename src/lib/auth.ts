791cbf5f8a3fbd5d69e84a74c07dd8f390be14c8
cesarlojan8 <cesarlojan8@gmail.com>
2025-10-27 01:15:51 +0000

    xd

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,30 +9,10 @@ import argon2 from "argon2";
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-
   database: prismaAdapter(prisma, {
     provider: 'mysql',
 
   }),
-  databaseHooks: {
-    account: {
-
-    }
-  },
-  
-  advanced: {
-    useSecureCookies: process.env.NODE_ENV === "production",
-
-    ipAddress: {
-      ipAddressHeaders: ['cf-connecting-ip']
-    }
-
-  },
-  trustedOrigins: [
-    "*",
-    "https://8787-firebase-aquafornite-1747146266938.cluster-iesosxm5fzdewqvhlwn5qivgry.cloudworkstations.dev/",
-    "https://aquafornais.dpdns.org/"
-  ],
   secret: process.env.BETTER_AUTH_SECRET,
   emailAndPassword: {
     enabled: true,
@@ -50,11 +30,5 @@ export const auth = betterAuth({
 
 
   },
-  plugins: [nextCookies()],
-  hooks: {
-    after: createAuthMiddleware(async (ctx) => {
-      const responseHeaders = ctx.context.responseHeaders;
-    })
-  }
-
+  plugins: [nextCookies()],  
 });
